399ea247c8c490efb43608fe80d6d273cedee638
Christopher Perry <c.perry@icloud.com>
2018-11-29 19:03:44 -0700

    First Commit

---
+++ script.js
@@ -0,0 +1,27 @@
+let newRepo = new XMLHttpRequest();
+newRepo.onreadystatechange = function(){
+    if(this.readyState == 4 && this.status == 200){
+        let myObj = JSON.parse(this.responseText);
+        for(var i=0; i < myObj.length; i++){
+            addRepo(myObj[i].name);
+        }
+    }
+};
+
+newRepo.open("GET","https://api.github.com/users/cperry1382/repos", true);
+newRepo.send();
+
+function addRepo(name){
+    var node = document.createElement("li");
+    var textNode = document.createTextNode(name);
+    node.appendChild(textNode);
+    document.getElementById("gitRepo").appendChild(node);
+}
+
+$(document).ready(function(){
+    $(".hover").hover(function(){
+        $(this).animate({height:'75px', width:'75px'});
+    }, 
+    function(){
+        $(this).animate({height:'34px', width: '34px'});
+    })})
